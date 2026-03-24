e585fa69c2841f355c955c4c6261866a9e00bdb7
Giovany Bal Roquel <balgiovany@gmail.com>
2026-03-24 09:36:40 -0600

    CARUSEL COMO CARD DE LOS DOCTORES

---
+++ script.js
@@ -0,0 +1,19 @@
+// Archivo: script.js
+alert("Hola mundo con Javascript")
+
+//var nombre = "giovany";
+var apellido = "bal";
+var presente = true;
+var ausente = false;
+var estarura = 1.75;
+var edad = 27;
+
+if (true) {
+    var nombre = "giovany";
+}
+
+console.log("Hola " + nombre + " "+ apellido); // Imprime "giovany"
+console.log(typeof(nombre)); // Imprime "string"
+console.log("tines " + edad + " años"); // Imprime "tines 27 años"
+
+console.log("Tu altura es " + estarura + " metros"); // Imprime "Mi altura es 1.75 metros"
